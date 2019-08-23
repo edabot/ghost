e2d0ddb5357d75a42c78589a1db050e86c3ddc8d
Ed Lewis <ed.lewis@gmail.com>
2019-08-23 19:08:02 -0400

    changes

--- ghost_flask.py
+++ ghost_flask.py
@@ -6,8 +6,10 @@ from tkinter import *
 from gpiozero import Servo, LED, Button
 from time import sleep
 from flask import request, Flask, render_template
+from random import randrange
 
-movie = ("/home/pi/Documents/ghost/ghost_sideways_2.mp4")
+movie_prefix = "/home/pi/Documents/ghost/movies/ghost_sideways_"
+movie_suffix = ".mp4"
 lensCover = Servo(17)
 projectorButton = LED(27)
 demoButton = Button(3)
@@ -15,7 +17,7 @@ ghostActive = False
 
 def play_movie():
     os.system('killall omxplayer.bin')
-    osmx = Popen(['omxplayer', '-b', movie])
+    osmx = Popen(['omxplayer', '-b', movie_prefix+str(randrange(1,4))+movie_suffix])
 
 def showGhost():
     ghostActive = True
@@ -24,10 +26,11 @@ def showGhost():
     sleep(1)
     projectorButton.off()
     sleep(3)
+    play_movie()
+    sleep(4)
     lensCover.max()
     sleep(2)
     lensCover.detach()
-    play_movie()
     sleep(6)
     projectorButton.on()
     sleep(1)
@@ -42,7 +45,8 @@ def api_root():
 
 @app.route('/playghost', methods=["GET"])
 def playGhost():
-    play_movie()
+    if not ghostActive:
+        showGhost()
     return render_template('index.html')
     
 if __name__ == "__main__":
